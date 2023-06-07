8f5d8009bc7f7890977f775ef5c551b18c6205c2
Frankdal <fra.dallagata@gmail.com>
2023-06-07 17:48:17 +0200

    fix: adding handling to the master channel exercise

--- lab4_1_master_condiviso.rs
+++ lab4_1_master_condiviso.rs
@@ -1,7 +1,4 @@
-use std::fmt::Error;
-use std::fs::canonicalize;
-use std::panic::catch_unwind;
-use std::sync::{Arc, Mutex, RwLock};
+use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{channel, Sender, Receiver};
 use std::thread;
 use std::thread::{JoinHandle, sleep};
@@ -11,9 +8,9 @@ use rand::{Rng};
 const N: usize = 3;
 
 struct CyclicBarrier {
-    sender_to_master: Mutex<Sender<i32>>,
+    sender_to_master: Mutex<Option<Sender<i32>>>,
     receivers_threads: Mutex<Vec<Receiver<i32>>>,
-    join_handle: JoinHandle<i32>
+    join_handle: Option<JoinHandle<i32>>
 }
 
 impl CyclicBarrier {
@@ -29,33 +26,40 @@ impl CyclicBarrier {
         let (sender_master, receiver_master) = channel();
         let mut join_handle : JoinHandle<i32> = thread::spawn({
             move || {
-                loop {
+                'a: loop {
                     let mut i = 0;
                     while i < N {
                         let res = receiver_master.recv();
-                        if res.is_err() { break };
+                        if res.is_err() {
+                            println!("Master is ending");
+                            break 'a;
+                        };
 
                         i += 1;
                     }
 
                     for i in 0..N {
                         let res = senders_to_threads[i].send(1);
-                        if res.is_err() { break };
+                        if res.is_err() {
+                            println!("Master is ending");
+                            break 'a;
+                        };
                     }
                 }
+                return 0
             }
         });
 
 
         return Arc::new(CyclicBarrier {
-            sender_to_master: Mutex::new(sender_master),
+            sender_to_master: Mutex::new(Some(sender_master)),
             receivers_threads,
-            join_handle
+            join_handle: Some(join_handle)
         });
     }
 
     fn wait(&self, index: usize) {
-        let sender_to_master = self.sender_to_master.lock().unwrap().clone();
+        let sender_to_master = self.sender_to_master.lock().unwrap().clone().unwrap();
         let receiver_thread = self.receivers_threads.lock().unwrap().pop().unwrap();
 
         println!("Thread {} has now stopped!", index);
@@ -69,10 +73,21 @@ impl CyclicBarrier {
 
         sleep(Duration::from_secs(rand::thread_rng().gen_range(2..5)));
     }
-    
-
 }
 
+
+impl Drop for CyclicBarrier{
+    fn drop(&mut self) {
+        *(self.sender_to_master.lock().unwrap()) = None;
+
+        // let mut handle : Option<JoinHandle<i32>> = None;
+        // std::mem::swap(&mut self.join_handle, &mut handle);
+        // handle.unwrap().join().unwrap();
+
+        let handle = self.join_handle.take();
+        handle.unwrap().join().unwrap();
+    }
+}
 
 fn main() {
     let a_barrier = CyclicBarrier::new();
@@ -94,4 +109,6 @@ fn main() {
     for h in join_handles {
         h.join().unwrap();
     }
+
+    println!("Program has terminated");
 }
