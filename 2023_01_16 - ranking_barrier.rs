de61c924ff16dd48205ea71ed169a1a32c53a410
MatteoGuarna <101059807+MatteoGuarna@users.noreply.github.com>
2023-08-03 09:31:24 +0200

    Update and rename ranking_barrier.rs to 2023_01_16 - ranking_barrier.rs

--- 2023_01_16 - ranking_barrier.rs
+++ 2023_01_16 - ranking_barrier.rs
@@ -1,3 +1,21 @@
+/*
+Una barriera è un costrutto di sincronizzazione usato per regolare l'avanzamento relativo della computazione di più thread. 
+All'atto della costruzione di questo oggetto, viene indicato il numero N di thread coinvolti. 
+
+Non è lecito creare una barriera che coinvolga meno di 2 thread. 
+
+La barriera offre un solo metodo, wait(), il cui scopo è bloccare temporaneamente l'esecuzione del thread che lo ha invocato, non ritornando fino a che non sono giunte 
+altre N-1 invocazioni dello stesso metodo da parte di altri thread: quando ciò succede, la barriera si sblocca e tutti tornano. Successive invocazioni del metodo wait() 
+hanno lo stesso comportamento: la barriera è ciclica.
+
+Attenzione a non mescolare le fasi di ingresso e di uscita!
+
+Una RankingBarrier è una versione particolare della barriera in cui il metodo wait() restituisce un intero che rappresenta l'ordine di arrivo: il primo thread ad avere 
+invocato wait() otterrà 1 come valore di ritorno, il secondo thread 2, e così via. All'inizio di un nuovo ciclo, il conteggio ripartirà da 1.
+
+Si implementi la struttura dati RankingBarrier a scelta nei linguaggi Rust o C++ '11 o successivi.
+*/
+
 use std::sync::{Arc, Condvar, Mutex, RwLock};
 
 const N: usize = 5;
