ee03f4a6ab35bd1809482a0efa65823e61db8cbf
MatteoGuarna <101059807+MatteoGuarna@users.noreply.github.com>
2023-08-05 19:09:58 +0200

    Update joiner_condvar.rs
    ALL GOOD

--- joiner_condvar.rs
+++ joiner_condvar.rs
@@ -35,7 +35,7 @@ const N_THREADS : usize = 3;
 
 struct Sensor{}
 
-impl Sensor{
+impl Sensor{    //each sensor is assigned to a thread and MUST be associated with a UNIQUE key (see comment at line 66)
     fn generate() -> f64{
         let ret: f64 = rand::thread_rng().gen();
         return ret
@@ -44,7 +44,7 @@ impl Sensor{
 
 struct Joiner <K: Hash + PartialEq + PartialOrd + Clone + Send, V: Clone + Send>{
     n_threads: usize,
-    map_plus_copy_counter: Mutex<(HashMap<K,V>, usize)>,
+    map_plus_copy_counter: Mutex<(HashMap<K,V>, usize)>, //counter is required to check that all threads copied the map before clearing it
     cv: Condvar
 }
 
@@ -59,21 +59,24 @@ impl<K: Hash + Eq + PartialEq + PartialOrd + Clone + Send, V: Clone + Send> Join
 
     fn supply(&self, key: K, value: V) -> HashMap<K,V>{
         let mut lock = self.map_plus_copy_counter.lock().unwrap();
-        lock = self.cv.wait_while(lock, |l|{ (*l).1 != 0}).unwrap();
+        lock = self.cv.wait_while(lock, |l|{ (*l).1 != 0}).unwrap(); //wait until each thread returned its own copy of the map (counter = 0 means program can proceed)
 
         (*lock).0.insert(key,value);
-        if (*lock).0.len() == self.n_threads {self.cv.notify_all()}
+        if (*lock).0.len() == self.n_threads {self.cv.notify_all()} //last to insert into map allows the map copy process to begin for all threads
+        //IT IS CRITICAL THAT EACH SENSOR IS IDENTIFIED BY ITS OWN UNIQUE ID, MEANING IF IN A ROUND OF MEASUREMENTS supply() RECIEVES MULTIPLE VALUES ATTACHED TO THE SAME
+        //kEY, THE VALUE IS OVERWRITTEN AND THE PROGRAM STARVES WAITING FOR ALLTHE CORRECT NUMBER OF MEASUREMENTS TO BE PROVIDED
+        //if you want to have the program working with duplicate keys too, another counter/flag must be provided (makes no sense having tuplicate key, therefore this solution assumes keys to be unique)
 
         lock = self.cv.wait_while(lock, |l|{ (*l).0.len() <  self.n_threads}).unwrap();
 
         let ret = (*lock).0.clone();
 
-        (*lock).1 += 1;
+        (*lock).1 += 1; //counter is needed to check that each thread cloned the map before clearing it
 
-        if (*lock).1 == self.n_threads {
+        if (*lock).1 == self.n_threads { //last thread to clone the map into ret clears the map
             (*lock).1 = 0;
             (*lock).0.clear();
-            self.cv.notify_all();
+            self.cv.notify_all(); //required to awake threads already waiting at line 62
         }
 
         return ret;
@@ -81,7 +84,7 @@ impl<K: Hash + Eq + PartialEq + PartialOrd + Clone + Send, V: Clone + Send> Join
 }
 
 
-fn main() {
+fn main() { //main is not required in the exam
     let barrier = Joiner::new(N_THREADS);
 
     let mut vt = Vec::new();
@@ -97,7 +100,7 @@ fn main() {
 
                         let v = Sensor::generate();
                         let map = b.supply(i, v);
-                        println!("\nMappa ritornata dal Thread #{i}\n{:?}\n", map);
+                        println!("\nMap returned by Thread #{i}\n{:?}\n", map);
                     }
                 }
             }
