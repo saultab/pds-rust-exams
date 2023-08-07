fc91368ecaedfe6ad9b1bfd1dd6b5f209fc05398
MatteoGuarna <101059807+MatteoGuarna@users.noreply.github.com>
2023-08-07 23:15:34 +0200

    ALL GOOD

--- joiner_channels.rs
+++ joiner_channels.rs
@@ -24,42 +24,51 @@ COMMENTO ALLA SOLUZIONE:
 Esercizio risolto utilizzando canali. Una soluzione alternativa è inclusa utilizzando mutex + condvar per ottenere la sincronizzazione.
 */
 
+
 use std::collections::HashMap;
 use std::fmt::{Display, Debug};
 use std::hash::Hash;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
 use std::thread::sleep;
 use rand::Rng;
 use std::time::Duration;
 
+const N_THREADS : usize = 3;
 
-const N:usize = 3;
+struct Sensor{}
 
+impl Sensor{    //each sensor is assigned to a thread and MUST be associated with a UNIQUE key (see comment at line 66)
+fn generate() -> i32{
+    let ret: i32 = rand::thread_rng().gen();
+    return ret
+}
+}
 
-struct Joiner<K: Hash + Eq + PartialEq + Clone + Display + Debug + Send + Ord, V: Clone + Display + Debug + Send + Ord > {
+struct Joiner<K: Hash + Eq + PartialEq + Clone + Display + Debug + Send + Ord, V: Clone + Display + Debug + Send + Ord> {
     number_threads: usize,
-    vec_sender: Mutex<Vec<Sender<(K,V)>>>,
-    vec_receiver: Mutex<Vec<Receiver<(K,V)>>>
+    vec_sender: Mutex<Vec<Sender<(K, V)>>>,
+    vec_receiver: Mutex<Vec<Receiver<(K, V)>>>,
 }
 
 impl<K: Hash + Eq + PartialEq + Clone + Display + Debug + Send + Ord, V: Clone + Display + Debug + Send + Ord> Joiner<K, V> {
-    fn new(number_threads: usize) -> Self {
+    fn new(number_threads: usize) -> Arc<Self> {
         let mut vec_sender = Vec::new();
         let mut vec_receiver = Vec::new();
-        for _ in 0..number_threads{
-            let (tx,rx) = channel();
+        for _ in 0..number_threads {
+            let (tx, rx) = channel();
             vec_sender.push(tx);
             vec_receiver.push(rx);
         }
-        Joiner {
+        Arc::new(Joiner {
             number_threads,
-            vec_sender:Mutex::new(vec_sender),
-            vec_receiver: Mutex::new(vec_receiver)
-        }
+            vec_sender: Mutex::new(vec_sender),
+            vec_receiver: Mutex::new(vec_receiver),
+        })
     }
 
-    fn supply(&self, k: K, v: V, index : usize) -> HashMap<K, V> {
+    fn supply(&self, k: K, v: V) -> HashMap<K, V> {
         let lock = self.vec_sender.lock().unwrap();
         let senders_vec = lock.clone();
         drop(lock);
@@ -68,14 +77,11 @@ impl<K: Hash + Eq + PartialEq + Clone + Display + Debug + Send + Ord, V: Clone +
         let receiver = lock.pop().unwrap();
         drop(lock);
 
-
         let mut map = HashMap::new();
 
-        println!("Thread {index} sending its pair and waiting...");
+        println!("Thread {k} sending its pair and waiting...");
         for i in 0..self.number_threads {
-            senders_vec[i].send((k.clone(),v.clone())).expect("Error");
-            //let rng: u64 = rand::thread_rng().gen_range(0..5);
-            //sleep(Duration::from_secs(rng));
+            senders_vec[i].send((k.clone(), v.clone())).expect("Error");
         }
 
         let mut dummy = Vec::new(); //this vec is necessary in order to guarantee BOTH ordering and values to remain the same across all maps
@@ -83,16 +89,15 @@ impl<K: Hash + Eq + PartialEq + Clone + Display + Debug + Send + Ord, V: Clone +
             let (k_rec, v_rec) = receiver.recv().expect("Error");
             dummy.push((k_rec, v_rec));
         }
-        //println!("#{index} Before sort {:?}", dummy);
+
         dummy.sort();
-        //println!("#{index} After sort {:?}", dummy);
 
         for i in 0..self.number_threads {
             let (k_rec, v_rec) = dummy[i].clone();
             map.insert(k_rec, v_rec);
         }
 
-        println!("Thread {index} can now resume!");
+        println!("Thread {k} can now resume!");
 
         let mut lock = self.vec_receiver.lock().unwrap();
         lock.push(receiver);
@@ -102,21 +107,23 @@ impl<K: Hash + Eq + PartialEq + Clone + Display + Debug + Send + Ord, V: Clone +
     }
 }
 
-fn main() {
-    let barrier = Arc::new(Joiner::new(N));
+fn main() { //main is not required in the exam
+    let barrier = Joiner::new(N_THREADS);
 
     let mut vt = Vec::new();
 
-    for i in 0..N {
-        vt.push(std::thread::spawn(
+    for i in 0..N_THREADS {
+        vt.push(thread::spawn(
             {
                 let b = barrier.clone();
                 move || {
-                    for _ in 0..3 {
-                        let rng: u64 = rand::thread_rng().gen_range(0..2);
+                    for _ in 0..5 {
+                        let rng: u64 = rand::thread_rng().gen_range(1..5);
                         sleep(Duration::from_secs(rng));
-                        let map = b.supply(rng, i, i);
-                        println!("\nMappa ritornata dal Thread #{i}\n{:?}\n", map);
+
+                        let v = Sensor::generate();
+                        let map = b.supply(i, v);
+                        println!("\nMap returned by Thread #{i}\n{:?}\n", map);
                     }
                 }
             }
